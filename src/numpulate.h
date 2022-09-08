927f607564f6305cf0968ddad03afc3f4595d819
Kiwifuit <mahkiwi123@gmail.com>
2022-09-08 21:49:56 +0800

    Added functions

--- src/numpulate.h
+++ src/numpulate.h
@@ -3,4 +3,19 @@
 #define __NUMPULATE_AUTHOR "Misery <mahkiwi123@gmail.com>"
 #define __NUMPULATE_VER "1.0.0"
 
+/// @brief Inverts a number's sign
+/// @param i The number to negate
+/// @return The negated number. This would be negative if 'i' was positive, and vice versa
+int negate(int i);
+
+/// @brief Multiplies i by two
+/// @param i The number to double
+/// @return The doubled number (i * 2)
+int i_doubled(int i);
+
+/// @brief Reverses a number (e.g. 123 would be 321)
+/// @param i The number to reverse
+/// @return THe reversed number
+int reverse(int i);
+
 #endif
