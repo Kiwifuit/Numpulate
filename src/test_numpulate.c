98e4db32b76ebad6bb03c52022820463b7bb718c
Kiwifuit <mahkiwi123@gmail.com>
2022-09-08 22:09:40 +0800

    Improved testing app

--- src/test_numpulate.c
+++ src/test_numpulate.c
@@ -1,6 +1,11 @@
 #include "numpulate.h"
+#include "stdio.h"
 
 int main()
 {
   int neg = negate(1);
+  int rev = reverse(21);
+
+  printf("1 negated is %i\n", neg);
+  printf("21 negated is %i\n", rev);
 }
